347172bc99110e373edaaeb678ca433db07db5fe
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-07-22 09:47:32 -0600

    chore: update database configuration variables
    The database configuration variables in the `db.config.js` file have been updated to use the new environment variable names (`DB_USER`, `DB_PASSWORD`, `DB_DATABASE`, `DB_HOST`). This change ensures consistency and clarity in the codebase.

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,10 +2,10 @@ import 'dotenv/config';
 import { createConnection as mysqlCreateConnection } from "mysql2/promise";
 
 const config = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST
 };
 
 const createConnection = async () =>{
