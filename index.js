d04bb446085ed341679da7088c55d6aae5330f11
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-07-25 00:58:30 -0600

    modified:   index.js

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config.js'
+import fs from 'fs'
+import https from 'https'
 import router from './src/routes/data.routes.js'
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const app = express();
 
+// Configuración de CORS
 app.use(cors({
     origin: "*",
     methods: ['GET'],
 }));
 
+// Parseo de JSON
 app.use(express.json());
 
-app.use('/datas', router)
+// Rutas
+app.use('/datas', router);
 
-app.listen(port||3006,() => {
-    console.log("Escuchando en el puerto 3006");
+// Cargar certificados SSL
+const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/hydrosense-peticions.integrador.xyz/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/hydrosense-peticions.integrador.xyz/fullchain.pem')
+};
+
+// Crear servidor HTTPS
+https.createServer(httpsOptions, app).listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
 });
